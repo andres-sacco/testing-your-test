7af181723ac6ccb42ba5b169f3d6244bf3534b49
Andres Sacco <andres.sacco@travelx.io>
2022-05-20 15:36:50 -0300

    feat: add validations

--- api-catalog/src/main/java/com/twa/flights/api/catalog/controller/CatalogController.java
+++ api-catalog/src/main/java/com/twa/flights/api/catalog/controller/CatalogController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.twa.flights.api.catalog.controller.documentation.CatalogResources;
 import com.twa.flights.api.catalog.dto.CityDTO;
 import com.twa.flights.api.catalog.dto.CountryDTO;
+import com.twa.flights.api.catalog.enums.ExceptionStatus;
+import com.twa.flights.api.catalog.exception.APIException;
 import com.twa.flights.api.catalog.service.CityService;
 import com.twa.flights.api.catalog.service.CountryService;
 
@@ -33,15 +35,33 @@ public class CatalogController implements CatalogResources {
     public ResponseEntity<CityDTO> getCityByCode(String code) {
         LOGGER.info("Obtain all the information about the city with code {}", code);
 
+        validateCity(code);
+        
         CityDTO response = cityService.getCityByCode(code);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+	private void validateCity(String code) {
+		if(code.length() != 3) {
+            throw new APIException(HttpStatus.NOT_FOUND, ExceptionStatus.CITY_NOT_FOUND.getCode(),
+                    ExceptionStatus.CITY_NOT_FOUND.getMessage());
+        }
+	}
+
     @Override
     public ResponseEntity<CountryDTO> getCountryByCode(String code) {
         LOGGER.info("Obtain all the information about the country with code {}", code);
+        
+        validateCountry(code);
 
         CountryDTO response = countryService.getCountryByCode(code);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+    
+	private void validateCountry(String code) {
+		if(code.length() != 2) {
+            throw new APIException(HttpStatus.NOT_FOUND, ExceptionStatus.COUNTRY_NOT_FOUND.getCode(),
+                    ExceptionStatus.COUNTRY_NOT_FOUND.getMessage());
+        }
+	}
 }
